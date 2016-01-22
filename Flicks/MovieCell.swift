74978ea9ded6bbd5469ddc54652b7d095786e124
Labuser <Labuser@wtk316-20.cec.wustl.edu>
2016-01-21 18:28:45 -0600

    Finished Base app

---
+++ Flicks/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  Flicks
+//
+//  Created by Labuser on 1/21/16.
+//  Copyright (c) 2016 David Ayeke. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
